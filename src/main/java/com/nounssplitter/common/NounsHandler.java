2beff75fd7e556870eb3d821fdb5b11737d28755
Wesley Galdino <wesleygaldino@gmail.com>
2016-06-09 03:11:38 -0300

    Finish NounsSplitter proof of concept

--- src/main/java/com/nounssplitter/common/NounsHandler.java
+++ src/main/java/com/nounssplitter/common/NounsHandler.java
@@ -8,10 +8,11 @@ import org.apache.commons.lang3.StringUtils;
 
 public class NounsHandler {
 
-    public static void process(String userInput, Scanner scanner) throws IllegalArgumentException {
+    public static void process(String userInput, Scanner scanner)
+        throws IllegalArgumentException {
+        LinkedHashSet<String> dictionary = null;
         if (!StringUtils.isBlank(userInput)) {
-            LinkedHashSet<String> dictionary = Utils.buildDictionary();
-//            System.out.println(dictionary);
+            dictionary = Utils.buildDictionary();
             StringBuilder tempTerm = new StringBuilder();
             ArrayList<String> nounsList = new ArrayList<String>();
 
@@ -21,15 +22,23 @@ public class NounsHandler {
                 Character letter = userInput.charAt(i);
                 // To avoid case sensitive issues
                 tempTerm.append(letter.toString().toLowerCase());
-//                System.out.println(tempTerm);
                 if (dictionary.contains(tempTerm.toString())) {
                     nounsList.add(tempTerm.toString());
-                    System.out.println("This noun exists: " + tempTerm);
                     tempTerm.setLength(0);
                 } else {
-//                    System.out.println("This noun doesn't exists: " + tempTerm);
                     continue;
                 }
+            }
+
+            if (!nounsList.isEmpty()
+                && Utils.hasSameSize(userInput, nounsList)) {
+                System.out.println("You have searched for the following nouns: "
+                                    + StringUtils.join(nounsList, ","));
+            } else {
+                System.out.println("Oops! There's something wrong...");
+                System.out.print("Please, retype the compound noun> ");
+                String newUserInput = scanner.nextLine();
+                process(newUserInput, scanner);
             }
         } else {
             System.out.print("What?! Please, next time type something, Bro! ¬¬ > ");
--- src/main/java/com/nounssplitter/common/Utils.java
+++ src/main/java/com/nounssplitter/common/Utils.java
@@ -2,34 +2,52 @@ package com.nounssplitter.common;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Utils {
 
+    private static LinkedHashSet<String> dictionaryLoaded = null;
+
     public static LinkedHashSet<String> buildDictionary() {
-        System.out.println("Loading german dictionary...");
+        LinkedHashSet<String> dictionary;
 
-        LinkedHashSet<String> dictionary = new LinkedHashSet<String>();
+        if (dictionaryLoaded == null) {
+            System.out.println("Loading german dictionary...");
 
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        File dictionaryFile = new File(classLoader
-                .getResource("dictionaries/german-common-nouns.txt")
-                .getFile()
-        );
+            dictionary = new LinkedHashSet<String>();
 
-        try (Scanner scanner = new Scanner(dictionaryFile)) {
-            while (scanner.hasNextLine()) {
-                String line = scanner.nextLine();
-                dictionary.add(line);
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            File dictionaryFile = new File(classLoader
+                    .getResource("dictionaries/german-common-nouns.txt")
+                    .getFile()
+            );
+
+            try (Scanner scanner = new Scanner(dictionaryFile)) {
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    dictionary.add(line);
+                }
+                scanner.close();
+                dictionaryLoaded = dictionary;
+                System.out.println("German dictionary loaded!");
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            scanner.close();
-            System.out.println("German dictionary loaded!");
-        } catch (IOException e) {
-            e.printStackTrace();
+        } else {
+            dictionary = dictionaryLoaded;
         }
 
         return dictionary;
+    }
+
+    public static boolean hasSameSize (String userInput,
+            ArrayList<String> nounsList) {
+        String nounsListJoined = StringUtils.join(nounsList, "");
+        return userInput.length() == nounsListJoined.length();
     }
 
 }
