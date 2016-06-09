aedf2ed4b78517fa4d4ac0ff07cc158c2ecb2218
Wesley Galdino <wesleygaldino@gmail.com>
2016-06-09 03:11:38 -0300

    Finish NounsSplitter proof of concept

--- src/main/java/com/nounssplitter/common/NounsHandler.java
+++ src/main/java/com/nounssplitter/common/NounsHandler.java
@@ -8,27 +8,31 @@ import org.apache.commons.lang3.StringUtils;
 
 public class NounsHandler {
 
+    private static int lastIndex = 0;
+
     public static void process(String userInput, Scanner scanner)
         throws IllegalArgumentException {
         LinkedHashSet<String> dictionary = null;
         if (!StringUtils.isBlank(userInput)) {
             dictionary = Utils.buildDictionary();
             StringBuilder tempTerm = new StringBuilder();
+            String lastNounFound = "";
             ArrayList<String> nounsList = new ArrayList<String>();
 
             System.out.println("\nSearching...");
 
-            for (int i = 0; i < userInput.length(); i++ ) {
-                Character letter = userInput.charAt(i);
-                // To avoid case sensitive issues
-                tempTerm.append(letter.toString().toLowerCase());
-                if (dictionary.contains(tempTerm.toString())) {
-                    nounsList.add(tempTerm.toString());
-                    tempTerm.setLength(0);
-                } else {
-                    continue;
-                }
-            }
+//            for (int i = 0; i < userInput.length(); i++ ) {
+//                Character letter = userInput.charAt(i);
+//                // To avoid case sensitive issues
+//                tempTerm.append(letter.toString().toLowerCase());
+//                if (dictionary.contains(tempTerm.toString())) {
+//                    nounsList.add(tempTerm.toString());
+//                    tempTerm.setLength(0);
+//                } else {
+//                    continue;
+//                }
+//            }
+            extractNounsFromTerm(0, userInput, dictionary, tempTerm, lastNounFound, nounsList);
 
             if (!nounsList.isEmpty()
                 && Utils.hasSameSize(userInput, nounsList)) {
@@ -44,6 +48,47 @@ public class NounsHandler {
             System.out.print("What?! Please, next time type something, Bro! ¬¬ > ");
             String newUserInput = scanner.nextLine();
             process(newUserInput, scanner);
+        }
+    }
+
+    private static void extractNounsFromTerm(int index, String userInput,
+            LinkedHashSet<String> dictionary, StringBuilder tempTerm,
+            String lastNounFound, ArrayList<String> nounsList) {
+        System.out.println("Index is: " + index);
+        if (index < userInput.length()) {
+//            System.out.println("Index is: " + index);
+            Character letter = userInput.charAt(index);
+            // To avoid case sensitive issues
+            tempTerm.append(letter.toString().toLowerCase());
+
+//            System.out.println("Lastindex is: " + lastIndex);
+            if (lastIndex != 0
+                    && index + 1 == userInput.length()) {
+                System.out.println("Here inside persistance layer");
+                int tempNextIndex = lastIndex + 1;
+                lastIndex = 0;
+//                System.out.println("Tempindex is: " + tempIndex);
+                nounsList.add(lastNounFound.toString());
+                System.out.println(nounsList);
+                tempTerm.setLength(0);
+                extractNounsFromTerm(tempNextIndex, userInput, dictionary, tempTerm, lastNounFound, nounsList);
+            }
+
+            System.out.println("tempTerm is: " + tempTerm.toString());
+            if (dictionary.contains(tempTerm.toString())) {
+                System.out.println("FOUND A MATCH");
+//                nounsList.add(tempTerm.toString());
+//                tempTerm.setLength(0);
+                lastNounFound = tempTerm.toString();
+                lastIndex = index;
+//                System.out.println("Lastindex is: " + lastIndex);
+                if (lastIndex < userInput.length()) {
+                    extractNounsFromTerm(index + 1, userInput, dictionary, tempTerm, lastNounFound, nounsList);
+                }
+            } else {
+                extractNounsFromTerm(index + 1, userInput, dictionary, tempTerm, lastNounFound, nounsList);
+            }
+
         }
     }
 
