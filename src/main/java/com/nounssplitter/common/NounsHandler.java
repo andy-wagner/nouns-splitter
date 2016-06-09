4e75da3184ad62058aa7f88f44d85e519653b89a
Wesley Galdino <wesleygaldino@gmail.com>
2016-06-09 03:11:38 -0300

    Finish NounsSplitter proof of concept

--- src/main/java/com/nounssplitter/common/NounsHandler.java
+++ src/main/java/com/nounssplitter/common/NounsHandler.java
@@ -37,17 +37,62 @@ public class NounsHandler {
             if (!nounsList.isEmpty()
                 && Utils.hasSameSize(userInput, nounsList)) {
                 System.out.println("You have searched for the following nouns: "
-                                    + StringUtils.join(nounsList, ","));
+                                    + StringUtils.join(nounsList, ", "));
             } else {
                 System.out.println("Oops! There's something wrong...");
                 System.out.print("Please, retype the compound noun> ");
                 String newUserInput = scanner.nextLine();
-                process(newUserInput, scanner);
+//                process(newUserInput, scanner);
             }
         } else {
             System.out.print("What?! Please, next time type something, Bro! ¬¬ > ");
             String newUserInput = scanner.nextLine();
-            process(newUserInput, scanner);
+//            process(newUserInput, scanner);
+        }
+    }
+
+    private static void extractNounsFromTerm(int index, String userInput,
+            LinkedHashSet<String> dictionary, StringBuilder tempTerm,
+            String lastNounFound, ArrayList<String> nounsList) {
+        if (index < userInput.length()) {
+            System.out.println("Index is: " + index);
+            Character letter = userInput.charAt(index);
+            // To avoid case sensitive issues
+            tempTerm.append(letter.toString().toLowerCase());
+
+//            System.out.println("Lastindex is: " + lastIndex);
+            if (lastIndex != 0
+                    && index == userInput.length() - 1 && lastIndex < userInput.length()) {
+                System.out.println("Here inside persistance layer");
+                int tempNextIndex = lastIndex + 1;
+                lastIndex = 0;
+//                System.out.println("Tempindex is: " + tempIndex);
+                nounsList.add(lastNounFound.toString());
+                System.out.println(nounsList);
+                tempTerm.setLength(0);
+                extractNounsFromTerm(tempNextIndex, userInput, dictionary, tempTerm, lastNounFound, nounsList);
+            }
+
+            System.out.println("tempTerm is: " + tempTerm.toString());
+            if (dictionary.contains(tempTerm.toString())) {
+                System.out.println("FOUND A MATCH");
+                lastNounFound = tempTerm.toString();
+                lastIndex = index;
+                if (lastIndex == userInput.length() - 1) {
+                    nounsList.add(lastNounFound.toString());
+                    System.out.println(nounsList);
+                    tempTerm.setLength(0);
+                } else {
+                    System.out.println("Lastindex is: " + lastIndex);
+                    extractNounsFromTerm(index + 1, userInput, dictionary, tempTerm, lastNounFound, nounsList);
+                }
+            } else {
+                if (lastIndex == userInput.length() - 1) {
+                    System.out.println("NOT FOUND A MATCH!");
+                }
+                extractNounsFromTerm(index + 1, userInput, dictionary, tempTerm, lastNounFound, nounsList);
+            }
+
         }
     }
 
